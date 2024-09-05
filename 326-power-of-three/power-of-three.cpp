bbb640b224c967317ff8acbb16fa76c154002aac
Jayasurya Panduru <121959583+jayasuryapanduru@users.noreply.github.com>
2024-09-05 15:24:39 +0530

    Time: 18 ms (28.30%) | Memory: 8.5 MB (25.76%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -2,7 +2,17 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
         
-        return n > 0 && static_cast<int>(pow(3, 19)) % n == 0;
+        //return n > 0 && static_cast<int>(pow(3, 19)) % n == 0;
+        if (n==0)
+        {
+            return false;
+        }
+        while (n%3==0)
+        {
+                n/=3;
+        }
+        return n==1;            
 
+        
     }
 };
