5885dca2ec32bb3bcca103f4ecda4e50e306ae58
Jayasurya Panduru <121959583+jayasuryapanduru@users.noreply.github.com>
2024-09-13 05:57:09 +0530

    Time: 11 ms (31.10%) | Memory: 13.6 MB (30.82%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+       const vector<pair<int, string>> valueSymbols{
+        {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"},
+        {90, "XC"},  {50, "L"},   {40, "XL"}, {10, "X"},   {9, "IX"},
+        {5, "V"},    {4, "IV"},   {1, "I"}};
+        string ans;
+        for(const auto &[val,sym]:valueSymbols)
+        {
+            if (num==0)
+            {
+                break;
+            }
+            while(num>=val)
+            {
+                num-=val;
+                ans+=sym;
+            }
+        }
+
+        return ans;
+    }
+};
