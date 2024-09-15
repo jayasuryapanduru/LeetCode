58f40167eb34a76324c8ec76b1f9f1f139ab2239
Jayasurya Panduru <121959583+jayasuryapanduru@users.noreply.github.com>
2024-09-15 18:08:18 +0530

    Time: 7 ms (95.23%) | Memory: 8.4 MB (93.39%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -4,28 +4,14 @@ public:
         
         int n = s.length();
         if (n<=1) return s;
-        // if (n<=2){
-
-        //     string l;
-        //     l+=s[0];
-        //     return l;
-        // }
         int st=0,e=0;
         int max_len = 0;
         for (int i=0;i<n-1;i++)
         {
             int l=i,r=i;
-            //n%2==1? r=i : r=i+1;
-            while(l>=0 && r<n)
+            while(l>=0 && r<n && s[l]==s[r]) 
             {
-                if (s[l]==s[r])
-                {
-                    l--,r++;
-                }
-                else 
-                {
-                    break;
-                }
+                l--,r++;
             }
             int len = r-l-1;
             if (len>max_len)
@@ -39,16 +25,10 @@ public:
         {
             int l=i,r=i+1;
             //n%2==1? r=i : r=i+1;
-            while(l>=0 && r<n)
+            while(l>=0 && r<n && s[l]==s[r])
             {
-                if (s[l]==s[r])
-                {
-                    l--,r++;
-                }
-                else 
-                {
-                    break;
-                }
+                 l--,r++;
+              
             }
             int len = r-l-1;
             if (len>max_len)
