13e47b98740387b4416135018a26d388228fd193
Jayasurya Panduru <121959583+jayasuryapanduru@users.noreply.github.com>
2024-12-13 06:52:47 +0530

    Time: 2 ms (97.51%) | Memory: 42.3 MB (9.68%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character>st = new Stack<>();
+
+        for(char c : s.toCharArray())
+        {
+            if (c=='(')
+            {
+                st.push(')');
+            }
+            else if (c=='[')
+            {
+                st.push(']');
+            }
+            else if (c=='{')
+            {
+                st.push('}');
+            }
+
+            else if (st.isEmpty() || st.pop()!=c)
+            {
+                return false;
+            }
+
+        }
+
+        return st.isEmpty();
+    }
+}
