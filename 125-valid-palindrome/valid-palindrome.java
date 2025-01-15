5f9809632b240dc0d4d738e912eb4a66f207f91a
Jayasurya Panduru <121959583+jayasuryapanduru@users.noreply.github.com>
2025-01-15 08:08:55 +0530

    Time: 2 ms (99.11%) | Memory: 42.2 MB (99.95%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -5,14 +5,20 @@ class Solution {
     int r = s.length() - 1;
 
     while (l < r) {
-      while (l < r && !Character.isLetterOrDigit(s.charAt(l)))
+
+        while (l < r && !Character.isLetterOrDigit(s.charAt(l)))
+        {
+            ++l;
+        }
+        while (l < r && !Character.isLetterOrDigit(s.charAt(r)))
+        {
+            --r;
+        }
+        if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r)))
+            return false;
         ++l;
-      while (l < r && !Character.isLetterOrDigit(s.charAt(r)))
         --r;
-      if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r)))
-        return false;
-      ++l;
-      --r;
+        
     }
 
     return true;
