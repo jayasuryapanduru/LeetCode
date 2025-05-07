300b0f7feb96b41cc897ab7f1a8c8cd42c941ffa
Jayasurya Panduru <121959583+jayasuryapanduru@users.noreply.github.com>
2025-05-07 12:07:49 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (71.35%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,14 +1,72 @@
 class Solution {
     public int missingNumber(int[] nums) {
         
-        int n = nums.length;
-        int sum1= n*(n+1)/2;
+
+        // optimal solution 
+
         int sum=0;
 
-        for(int i:nums)
+        for (int i=1;i<=nums.length;i++)
         {
             sum+=i;
         }
-        return sum1-sum;
+        int s2 = 0;
+        for (int i=0;i<nums.length;i++)
+        {
+            s2+=nums[i];
+        }
+        return Math.abs(sum-s2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Brute force Solution 
+
+        /*
+        int max=0;
+        for (int i:nums)
+        {
+            if (max<=i)
+            {
+                max = i;
+            }
+        }
+        System.out.print(max);
+        int k=0;
+        for (int i=1;i<=max;i++)
+        {
+            int flag = 0;
+            for (int j=0;j<nums.length-1;j++)
+            {
+                if (nums[j]==i)
+                {
+                    flag=1;
+                    break;
+                }
+            }
+            if (flag==0)
+            {
+                k = i;
+            }
+        }
+        return k;
+        */
     }
 }
